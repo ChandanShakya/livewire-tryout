b9d8a677cda9b97e9bed307deef93ccf33867be5
ChandanShakya <chandan.shakya@nccs.edu.np>
2025-01-03 14:51:46 +0545

    Implement session persistence for counter and add Laravel Debugbar dependency

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -1,13 +1,15 @@
 <?php
 namespace App\Livewire;
 
+use Livewire\Attributes\Session;
 use Livewire\Component;
 use Livewire\Attributes\Title;
 
 #[Title('Counter')]
 class Counter extends Component
 {
-    public $counter = 0;
+    #[Session(key: 'counter')]
+    public $counter;
 
     public function increment()
     {
